6bfaaeaa2263ce83ffa742a1475024613e4a0f88
will9183 <mtby2016@gmail.com>
2017-08-20 17:04:01 -0400

    Updated main.py
    Cleaned up the file a bit.

--- main.py
+++ main.py
@@ -1,27 +1,15 @@
 from guizero import App, PushButton, MenuBar, info
+import com
 import pygame
-import os
 pygame.mixer.init()
-def exit():
-    os._exit(0)
-def about():
-    info ("About", "Druaga1 Soundboard v0.01a by Will9183.")
-def ssd1():
-    pygame.mixer.music.load("ssd1.wav")
-    pygame.mixer.music.play()
-def ssd2():
-    pygame.mixer.music.load("ssd2.wav")
-    pygame.mixer.music.play()
-def testb3():
-    print("Oh, more buttons")
 app = App(title="Druaga1 Soundboard", layout="grid")
 menubar = MenuBar(app,
                   toplevel=["File", "Help"],
                   options=[
-                      [ ["Exit", exit] ],
-                      [ ["About", about] ]
+                      [ ["Exit", com.exit] ],
+                      [ ["About", com.about], ["View Code", com.viewcode] ]
                       ] )
-ssd1 = PushButton(app, command=ssd1, text="SSD1", grid=[0,1])
-ssd2 = PushButton(app, command=ssd2, text="SSD2", grid=[0,2])
-button3 = PushButton(app, command=testb3, text="Test2", grid=[1,1])
+ssd1 = PushButton(app, command=com.ssd1, text="SSD1", grid=[0,1])
+ssd2 = PushButton(app, command=com.ssd2, text="SSD2", grid=[0,2])
+button3 = PushButton(app, command=com.testb3, text="Test2", grid=[1,1])
 app.display()
